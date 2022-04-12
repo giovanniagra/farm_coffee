5ca2cb4b0edebc686b006acd8cc254b4f733c699
Tarun <taruntudu.tudu@gmail.com>
2022-04-13 00:16:58 +0700

    Added sign-in & sign-up + modified urls
    Co-authored-by: giovanniagra <giovanniagra@users.noreply.github.com>

--- farm_coffee/urls.py
+++ farm_coffee/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from farm_coffee_app import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('menu', views.menu, name='menu')
+    path('', include('farm_coffee_app.urls'))
 ]
