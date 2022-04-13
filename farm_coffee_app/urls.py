0091804a44b678c280711103853068bbe9992a43
Tarun <taruntudu.tudu@gmail.com>
2022-04-13 17:24:17 +0700

    Made login/sign-up
    Co-authored-by: giovanniagra <giovanniagra@users.noreply.github.com>

--- farm_coffee_app/urls.py
+++ farm_coffee_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .import views
 
+app_name = 'farm_coffee_app'
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('menu', views.menu),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
+    path("register", views.register_request, name='register'),
+    path("login", views.login_request, name="login")
 ]
--- farm_coffee_app/views.py
+++ farm_coffee_app/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
+
+
+
 
 # Create your views here.
 def home(request):
@@ -7,11 +15,37 @@ def home(request):
 def menu(request):
     return render(request, 'menu.html', {})
 
-def signup(request):
-    return render(request, "signup.html", {})
+def register_request(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("farm_coffee_app:home")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = SignUpForm()
+    return render (request=request, template_name='register.html', context={"register_form":form})
 
-def signout(request):
-    return render(request, "signout.html", {})
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("farm_coffee_app:home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.eror(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form":form})
 
-def signup(request):
-    pass
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("farm_coffee_app:home")
